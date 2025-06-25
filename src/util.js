4e970ff876d0ad8f97be9e5f2f825b29d2aed438
Joseph Mawa <186199059+josephmawa@users.noreply.github.com>
2025-06-25 21:33:05 +0300

    feat: add insert-text event handler

--- src/util.js
+++ src/util.js
@@ -1,3 +1,8 @@
 import Gio from "gi://Gio";
 
 export const settings = Gio.Settings.new(pkg.name);
+export const regexes = {
+  validEntry: /^[+-]?(\d+(\.\d*)?(e[+-]?\d*)?|\.\d+(e[+-]?\d*)?)?$/,
+  validNumber: /^-?\d*\.?\d+(?:[eE][+-]?\d+)?$/,
+  validDigit: /[-+eE0-9\.]/,
+};
--- src/win.js
+++ src/win.js
@@ -4,13 +4,18 @@ import Gio from "gi://Gio";
 import GLib from "gi://GLib";
 import GObject from "gi://GObject";
 
-import { settings } from "./util.js";
+import { settings, regexes } from "./util.js";
 
 export const GaugeWindow = GObject.registerClass(
   {
     GTypeName: "GaugeWindow",
     Template: getResourceURI("win.ui"),
-    InternalChildren: ["split_view", "search_bar"],
+    InternalChildren: [
+      "split_view",
+      "search_bar",
+      "input_entry",
+      "output_entry",
+    ],
   },
   class GaugeWindow extends Adw.ApplicationWindow {
     constructor(application) {
@@ -21,6 +26,21 @@ export const GaugeWindow = GObject.registerClass(
       this.createActions();
       this.setColorScheme();
       this.createColorSchemeAction();
+
+      /**
+       * NOTE
+       * You can't connect insert-text event to Gtk.Entry directly.
+       * Read more about it in the following reference docs:
+       * • https://gitlab.gnome.org/GNOME/gtk/-/issues/4315
+       * • https://docs.gtk.org/gtk4/iface.Editable.html#implementing-gtkeditable
+       */
+      this._input_entry
+        .get_delegate()
+        .connect("insert-text", this.insertTextHandler);
+
+      this._output_entry
+        .get_delegate()
+        .connect("insert-text", this.insertTextHandler);
     }
 
     handleSearch() {
@@ -92,6 +112,71 @@ export const GaugeWindow = GObject.registerClass(
         cssProvider,
         Gtk.STYLE_PROVIDER_PRIORITY_USER
       );
+    };
+
+    wordChanged(entry) {
+      console.log(entry instanceof Gtk.Entry);
+      entry.set_position(-1);
+    }
+
+    insertTextHandler = (editable, text, length, position) => {
+      const signalId = GObject.signal_lookup("insert-text", editable);
+      const handlerId = GObject.signal_handler_find(
+        editable,
+        GObject.SignalMatchType.ID,
+        signalId,
+        GLib.quark_to_string(0),
+        null,
+        null,
+        null
+      );
+      GObject.signal_handler_block(editable, handlerId);
+
+      if (regexes.validDigit.test(text)) {
+        /**
+         * We only allow these characters: /[-+eE0-9]/.
+         * Therefore, the text entry will contain ASCII
+         * and the spread syntax will split the string
+         * into actual characters.
+         */
+        const characters = [...editable.text];
+        const cursorPosition = editable.get_position();
+        characters.splice(cursorPosition, 0, text);
+        const bufferText = characters.join("");
+
+        if (regexes.validEntry.test(bufferText)) {
+          editable.insert_text(text, text.length, cursorPosition);
+        }
+      }
+
+      GObject.signal_handler_unblock(editable, handlerId);
+      GObject.signal_stop_emission(editable, signalId, GLib.quark_to_string(0));
+    };
+
+    createToast = (timeout = 1) => {
+      this.toast = new Adw.Toast({ timeout });
+    };
+
+    displayToast = (message) => {
+      this.toast.dismiss();
+      this.toast.title = message;
+      this._toast_overlay.add_toast(this.toast);
+    };
+
+    debounce = (callback, wait = 300) => {
+      let debounceTimeout = null;
+
+      return (...args) => {
+        if (debounceTimeout) {
+          GLib.source_remove(debounceTimeout);
+        }
+
+        debounceTimeout = GLib.timeout_add(GLib.PRIORITY_DEFAULT, wait, () => {
+          callback(...args);
+          debounceTimeout = null;
+          return GLib.SOURCE_REMOVE;
+        });
+      };
     };
   }
 );
